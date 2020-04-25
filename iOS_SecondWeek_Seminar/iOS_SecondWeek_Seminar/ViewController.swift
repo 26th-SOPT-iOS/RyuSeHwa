50a3cd17ab587212f3b5dd528d5fd7e4e35878c2
Sehwa Ryu <sehwaryu@Sehwaui-MacBookPro.local>
2020-04-25 17:38:58 +0900

    Second Week

---
+++ iOS_SecondWeek_Seminar/iOS_SecondWeek_Seminar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_SecondWeek_Seminar
+//
+//  Created by Sehwa Ryu on 25/04/2020.
+//  Copyright © 2020 Sehwa Ryu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
